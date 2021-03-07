8957288bc57eef31542f7c3c5effcd0dcee252d3
Jonell Balanay <jonellbalanay@developersinstitute.co.nz>
2021-03-07 21:24:35 +1300

    Add User register request handler

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,14 +1,36 @@
 import { Router } from 'express';
-import User from '..models/userModel';
+import User from '../models/userModel';
 
-const Router = Router();
+const router = Router();
 
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
   try {
     const { body } = req;
     console.log(body);
+    if (
+      !body.hasOwnProperty('username') ||
+      !body.hasOwnProperty('email') ||
+      !body.hasOwnProperty('password')
+    ) {
+      return res
+        .status(400)
+        .json({ error: 'Username, email, password required!' });
+    }
+    const { username, password, email } = body;
 
-    return res.end();
+    const checkUsername = await User.findOne({ username });
+    if (checkUsername) {
+      return res.status(400).json({ error: 'Username already taken!' });
+    }
+    const checkEmail = await User.findOne({ email });
+    if (checkEmail) {
+      return res.status(400).json({ error: 'Email address already in use!' });
+    }
+
+    const user = new User(body);
+    await user.save();
+
+    return res.status(201).json({ success: true });
   } catch (e) {
     next(e);
   }
