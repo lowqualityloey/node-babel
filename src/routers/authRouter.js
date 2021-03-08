ada5eb32eeebfebcf51bed01ee21140460281e94
Jonell Balanay <jonellbalanay@developersinstitute.co.nz>
2021-03-09 04:32:54 +1300

    Adding auth register validation

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,23 +1,17 @@
 import { Router } from 'express';
 import argon2 from 'argon2';
 import User from '../models/userModel';
+import registerSchema from '../validation/authRegisterUser';
 
 const router = Router();
 
 router.post('/register', async (req, res, next) => {
   try {
     const { body } = req;
-    console.log(body);
-    if (
-      !body.hasOwnProperty('username') ||
-      !body.hasOwnProperty('email') ||
-      !body.hasOwnProperty('password')
-    ) {
-      return res
-        .status(400)
-        .json({ error: 'Username, email, password required!' });
-    }
-    const { username, password, email } = body;
+    const validValues = await registerSchema.validateAsync(body);
+    console.log('validValues', validValues);
+
+    const { username, password, email } = validValues;
 
     const checkUsername = await User.findOne({ username });
     if (checkUsername) {
@@ -36,6 +30,10 @@ router.post('/register', async (req, res, next) => {
 
     return res.status(201).json({ success: true });
   } catch (e) {
+    //catch custom validation errors
+    if (e.message.startsWith('Invalid')) {
+      return res.status(400).json({ error: e.message });
+    }
     next(e);
   }
 });
---
+++ src/validation/authRegisterUser.js
@@ -0,0 +1,24 @@
+import Joi from 'joi';
+
+const schema = Joi.object({
+  username: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(50)
+    .required()
+    .error(new Error('Invalid username - must be 3 and 50 characters.')),
+
+  password: Joi.string()
+    .pattern(new RegExp('^[a-zA-Z0-9]{6,64}$'))
+    .required()
+    .error(new Error('Invalid password - must be at least 6 characters long.')),
+
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+    })
+    .required()
+    .error(new Error('Invalid email - must be a valid email')),
+});
+
+export default schema;
